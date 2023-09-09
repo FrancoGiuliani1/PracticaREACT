a840e3d2de0aa10ab3fc83870b9c181fd923bbe1
FrancoGiuliani1 <francogiulianiii@gmail.com>
2023-09-09 16:05:10 -0300

    agrego funciones para agregar tareas, eventos y mas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './Componentes/TodoList';
+import TodoForm from './Componentes/TodoForm';
+import TodoItem from './Componentes/TodoItem';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Mi primera aplicacion React</h1>
-      <p>Bienvenido al mundo react.</p>
-    </div>
 
-  );
+  const [tasks, setTasks] = useState([]); //task: indica el estado actual de las tareas.
+  //setTask: funcion que permite actualizar el estado de Task
+  const [taskText, setTaskText] = useState (''); 
+  
+  const handleTaskTextChange = (event) => {
+       setTaskText(event.target.value);  
+  }
+  const addTask = () => {
+    if (taskText.trim() !== ''){
+     setTasks ([...tasks, taskText]); //Agrega la nueva tarea al array
+     setTaskText('');//limpia la entrada del campo para ingresar tareas nuevas
+
+    }
+  };
+function hola (){
+  <h1>Hola</h1>
+};
 }
 
 export default App;
